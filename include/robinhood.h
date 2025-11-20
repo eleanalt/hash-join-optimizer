fa18ce42162dbf9e455185bf4b6efef083ac69ee
Alex <sdi2100174@di.uoa.gr>
2025-11-20 19:18:59 +0200

    Set default capacity to max size

--- include/robinhood.h
+++ include/robinhood.h
@@ -18,7 +18,7 @@ private:
         unsigned int psl = 0;
         bool occupied = false;
     };
-    static constexpr size_t DEFAULT_CAPACITY = 32768;
+    static constexpr size_t DEFAULT_CAPACITY = 4194304; // 2^22 , max size
     static constexpr double MAX_LOAD_FACTOR = 0.9;
 
     size_t capacity = DEFAULT_CAPACITY;
